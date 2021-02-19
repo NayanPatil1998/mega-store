4114d4b50f2668157b8ad5e32d0b52072ea15518
Nayan Patil <patil.nayan04@gmail.com>
2021-02-19 23:56:25 +0530

    Fix: Server: update user controller

--- server/src/Controllers/User.controller.ts
+++ server/src/Controllers/User.controller.ts
@@ -1,46 +1,33 @@
 import { Request, Response } from "express";
 import User, { IUser } from "../Models/user.model";
-import {auth} from 'firebase-admin'
 
 export const allUser = async (req: Request, res: Response) => {
-    const users: IUser[] = await User.find({},(err) => console.error(err))
+  const users: IUser[] = await User.find({}, (err) => console.error(err));
 
-    res.json(users)
-}
+  res.json(users);
+};
 
 export const addUser = async (req: Request, res: Response) => {
-  const { name, email, phone, password } = req.body;
+  const { name, email, phone, uid } = req.body;
 
-  console.log(email)
-
-  await auth()
-  .createUser({
+  console.log(email);
+  try {
+    const user: IUser = new User({
+      name: name,
       email: email,
-      phoneNumber:phone,
-      emailVerified: false,
-      displayName: name,
-      password : password,
-      disabled: false
-  }).then((userData) => {
-    const user : IUser = new User({
-        name: name,
-        email: email,
-        phone: phone,
-        uid: userData.uid
-    })
-
+      phone: phone,
+      uid: uid,
+    });
 
     user.save((err: any) => {
-        if(err){
-            res.status(500).send(err)
-        }else{
-            res.status(200).send(userData)
-        }
-    })
-
-  })
-  .catch((err) => {
-      res.status(500).send(err.message)
-  })
-//   res.json(user);
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(user);
+      }
+    });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+  //   res.json(user);
 };
