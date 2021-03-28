33bd86029819b2fe1284cd641e8c0d7b7f66ae44
Nayan Patil <patil.nayan04@gmail.com>
2021-03-28 23:43:44 +0530

    Changed the description type to array of product model

--- server/src/Models/product.model.ts
+++ server/src/Models/product.model.ts
@@ -1,46 +1,50 @@
 import Mongoose, { Document, Schema } from "mongoose";
 
-export interface IProductModel extends Document{
-    _id: string;
-    title: string;
-    price: number;
-    description : string;
-    category: string;
-    image: string;
-    date: Date;
-    feat: boolean;
+export interface IProductModel extends Document {
+  _id: string;
+  title: string;
+  price: number;
+  description: Array<string>;
+  category: string;
+  image: string;
+  date: Date;
+  feat: boolean;
 }
 
-const productSchema : Schema = new Schema({
-    title : {
-        type:String,
-        required: true
-    },
-    price : {
-        type:Number,
-        required: true
-    },
-    description : {
-        type:String,
-        required: true
-    },
-    category : {
-        type:String,
-        required: true
-    },
-    image : {
-        type:String,
-        required: true
-    },
-    feat :{
-      type: Boolean,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now(),
-    },
-})
+const productSchema: Schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: Array,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  feat: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-const Product = Mongoose.model<IProductModel>("Product", productSchema, "Products")
+const Product = Mongoose.model<IProductModel>(
+  "Product",
+  productSchema,
+  "Products"
+);
 export default Product;
