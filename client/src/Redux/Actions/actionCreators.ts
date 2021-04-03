ac318be0d85de85cbc25389ca4155199a0799e0f
Nayan Patil <patil.nayan04@gmail.com>
2021-04-03 23:26:21 +0530

    cartpage redux update

--- client/src/Redux/Actions/actionCreators.ts
+++ client/src/Redux/Actions/actionCreators.ts
@@ -1,5 +1,12 @@
 import firebase from "firebase";
-import { ADD_TO_CART, SET_USER, USER_LOADING } from "./actionTypes";
+import {
+  ADD_TO_CART,
+  DECREMENT_PRODUCT_QUAN,
+  INCREMENT_PRODUCT_QUAN,
+  REMOVE_FROM_CART,
+  SET_USER,
+  USER_LOADING,
+} from "./actionTypes";
 import { Action } from "../Reducers/type";
 import IProduct from "../../Components/ProductContainer/Product/product.type";
 
@@ -22,6 +29,29 @@ export const addToCart = (product: IProduct | undefined) => {
   const action: Action = {
     type: ADD_TO_CART,
     payload: product,
+  };
+  return action;
+};
+
+export const incrementProductQuantity = (index: number) => {
+  const action: Action = {
+    type: INCREMENT_PRODUCT_QUAN,
+    payload: index,
+  };
+  return action;
+};
+export const decrementProductQuantity = (index: number) => {
+  const action: Action = {
+    type: DECREMENT_PRODUCT_QUAN,
+    payload: index,
+  };
+  return action;
+};
+
+export const removeFromCart = (index: number) => {
+  const action: Action = {
+    type: REMOVE_FROM_CART,
+    payload: index,
   };
   return action;
 };
--- client/src/Redux/Reducers/index.ts
+++ client/src/Redux/Reducers/index.ts
@@ -1,12 +1,18 @@
 import firebase from "firebase";
-import {ADD_TO_CART, SET_USER} from "../Actions/actionTypes";
-import {Action, CartItem} from "./type";
+import {
+  ADD_TO_CART,
+  DECREMENT_PRODUCT_QUAN,
+  INCREMENT_PRODUCT_QUAN,
+  REMOVE_FROM_CART,
+  SET_USER,
+} from "../Actions/actionTypes";
+import { Action, CartItem } from "./type";
 import IProduct from "../../Components/ProductContainer/Product/product.type";
 
 export interface IinitialState {
   isUserLoading: boolean;
   user: firebase.User | null;
-  cart: CartItem[]
+  cart: CartItem[];
 }
 
 const initialState: IinitialState = {
@@ -16,16 +22,14 @@ const initialState: IinitialState = {
 };
 
 export const totalFun = (total: number, cartItem: CartItem) => {
-  return total +(cartItem.quantity * cartItem.product.price);
-}
-
-
-
+  return total + cartItem.quantity * cartItem.product.price;
+};
 
 const reducer = (
   state: IinitialState = initialState,
   action: Action
 ): IinitialState => {
+  let newCart = [...state.cart];
   switch (action.type) {
     case SET_USER:
       return {
@@ -33,27 +37,48 @@ const reducer = (
         user: action.payload,
       };
     case ADD_TO_CART:
-      let isExist = false
-      let newCart = [...state.cart]
-          newCart.forEach((item, index) => {
-        if(item.product._id === action.payload._id) {
+      let isExist = false;
+      newCart.forEach((item, index) => {
+        if (item.product._id === action.payload._id) {
           isExist = true;
           item.quantity++;
-          return false
-        }else{
+          return false;
+        } else {
           isExist = false;
         }
-      })
-      console.debug(newCart)
-      return isExist ? {
+      });
+      console.debug(newCart);
+      return isExist
+        ? {
             ...state,
-            cart: newCart
-          } :
-          {
-            ...state,
-            cart: [...state.cart, {product: action.payload, quantity: 1}]
+            cart: newCart,
           }
+        : {
+            ...state,
+            cart: [...state.cart, { product: action.payload, quantity: 1 }],
+          };
+    case INCREMENT_PRODUCT_QUAN:
+      newCart[action.payload].quantity++;
+      return {
+        ...state,
+        cart: newCart,
+      };
 
+    case DECREMENT_PRODUCT_QUAN:
+      let newCarts = [...state.cart];
+
+      newCarts[action.payload].quantity--;
+      return {
+        ...state,
+        cart: newCarts,
+      };
+
+    case REMOVE_FROM_CART:
+      newCart.splice(action.payload, 1);
+      return {
+        ...state,
+        cart: newCart,
+      };
 
     default:
       return state;
