50fbe14f5f922e221bef4de6f87f7dbe500c0839
Nayan Patil <patil.nayan04@gmail.com>
2021-04-18 22:20:37 +0530

    Intrgrated Stripe in payment controller

--- server/src/Controllers/payment.controller.ts
+++ server/src/Controllers/payment.controller.ts
@@ -1,7 +1,25 @@
 import {Request, Response} from "express";
+import { stripe } from "../server";
 
-export const createPaymentIntent = (req: Request, res: Response) => {
-    const {email, amount} = req.query;
-    console.info(email, amount);
-    res.json({})
+export const  createPaymentIntent = async (req: Request, res: Response) => {
+
+    if(!req.query.amount || !req.query.email ){
+        res.status(401);
+    }
+    const amount = req.query.amount !;
+    const email = req.query.email !;
+
+    console.log(amount)
+
+    const paymentintent = await stripe.paymentIntents.create({
+        currency: 'inr',
+        amount: +amount * 100,
+        payment_method_types: ['card'],
+        receipt_email: email.toString()
+    }).then((response) => {
+        console.log(response.client_secret);
+        res.status(200).json({
+            secret_id: response.client_secret,
+        })
+    })
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,16 @@ import express from "express";
 import cors from "cors";
 import initdb from "./database/db";
 import usersRouter from "./Routes/user.routes";
+import Stripe from 'stripe';
+
+export const stripe = new Stripe('sk_test_51HPvUxDNkye9sjaqNRwxmLgPou6XHl8QjdZcY3T5i6KNjj08uPpsLsmLVskLsoOjHnDAKU26hdUiKFC78hK9fMZE00ggGe05IN', {
+    apiVersion: '2020-08-27',
+});
+
+
 var morgan = require("morgan");
+
+
 require("dotenv").config();
 import * as admin from "firebase-admin";
 import productRouter from "./Routes/product.routes";
