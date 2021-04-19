bab16a8b21e33acadc190c4be0269762e4698cbb
Nayan Patil <patil.nayan04@gmail.com>
2021-04-19 20:33:01 +0530

    added address in OrderSchema and conroller

--- server/src/Controllers/Order.controller.ts
+++ server/src/Controllers/Order.controller.ts
@@ -1,28 +1,26 @@
-import {Request, Response} from "express";
-import {IOrder} from "../Models/Order";
+import { Request, Response } from "express";
+import { IOrder } from "../Models/Order";
 import Order from "../Models/Order";
 
 export const addOrder = async (req: Request, res: Response) => {
-    const { orderId, status, amount, products, date } = req.body;
-    try {
-        const order: IOrder = new Order(
-            {
-                orderID: orderId,
-                status: status,
-                amount: amount,
-                products: products,
-                date: date
-            }
-        );
-        order.save(err => {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.status(200).send(order);
-            }
-        })
-    }catch (e) {
-        res.status(500).send(e.message);
-
-    }
-}
+  const { orderId, status, amount, products, date, address } = req.body;
+  try {
+    const order: IOrder = new Order({
+      orderID: orderId,
+      status: status,
+      amount: amount,
+      products: products,
+      address: address,
+      date: date,
+    });
+    order.save((err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(order);
+      }
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+};
--- server/src/Models/Order.ts
+++ server/src/Models/Order.ts
@@ -1,48 +1,46 @@
-import Mongoose, { Schema, Document} from "mongoose";
-import {IProductModel} from "./product.model";
+import Mongoose, { Schema, Document } from "mongoose";
+import { IProductModel } from "./product.model";
 
-export type CartItem  = {
-    product: IProductModel,
-    quantity: number
-}
+export type CartItem = {
+  product: IProductModel;
+  quantity: number;
+};
 export interface IOrder extends Document {
-    _id: string,
-    orderId: string,
-    status: string,
-    amount: string,
-    products: CartItem[]
+  _id: string;
+  orderId: string;
+  status: string;
+  amount: string;
+  address: string;
+  products: CartItem[];
 }
 
+const OrderSchema: Schema = new Schema({
+  orderID: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  products: {
+    type: Array,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+});
 
-const OrderSchema: Schema = new Schema(
-    {
-        orderID: {
-            type: String,
-            required: true
-        },
-        status: {
-            type: String,
-            required: true
-        },
-        products :{
-            type: Array,
-            required: true
-        },
-        amount: {
-            type: String,
-            required : true
-        },
-        date: {
-            type: Date,
-            required: true
-        }
-    }
-)
-
-const Order = Mongoose.model<IOrder>(
-    "Order",
-    OrderSchema,
-    "Orders"
-);
+const Order = Mongoose.model<IOrder>("Order", OrderSchema, "Orders");
 
 export default Order;
