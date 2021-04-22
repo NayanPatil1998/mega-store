d17adc777c4f11a7fc5c7f2b61ba50f683beb955
Nayan Patil <patil.nayan04@gmail.com>
2021-04-22 23:55:16 +0530

    order route fixed

--- server/src/Routes/orders.routes.ts
+++ server/src/Routes/orders.routes.ts
@@ -1,10 +1,9 @@
 import express from "express";
-import {addOrder} from "../Controllers/Order.controller";
-import {checkIfAuthenticated} from "../middlewares/userMiddleware";
+import { addOrder } from "../Controllers/Order.controller";
+import { checkIfAuthenticated } from "../middlewares/userMiddleware";
 
 const ordersRoutes = express.Router();
 
-ordersRoutes.post("/orders", checkIfAuthenticated , addOrder);
-
+ordersRoutes.post("/orders", checkIfAuthenticated, addOrder);
 
 export default ordersRoutes;
